e919524564ba36262af73bcac80b580366d2e40f
vinithkumar <babluvinith25@gmail.com>
2022-02-08 14:04:33 +0530

    clear bugs

--- src/main/webapp/assets/js/addBill.js
+++ src/main/webapp/assets/js/addBill.js
@@ -12,10 +12,10 @@
 			today = yyyy + '-' + mm + '-' + dd;
 			document.getElementById("servdate").setAttribute("max", today);
 			
-			var today = new Date();
-			var dd = today.getDate();
-			var mm = today.getMonth() + 1;
-			var yyyy = today.getFullYear();
+			today = new Date();
+		    dd = today.getDate();
+		    mm = today.getMonth() + 1;
+		    yyyy = today.getFullYear();
 			if (dd < 10) {
 				dd = '0' + dd
 			}
