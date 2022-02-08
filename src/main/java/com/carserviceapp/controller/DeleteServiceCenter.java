1d317d26440b9085e84181dcc40ec5a73d09b0b1
vinithkumar <babluvinith25@gmail.com>
2022-02-08 15:06:55 +0530

    clear bugs

--- src/main/java/com/carserviceapp/controller/DeleteServiceCenter.java
+++ src/main/java/com/carserviceapp/controller/DeleteServiceCenter.java
@@ -17,39 +17,33 @@ import com.carserviceapp.model.CenterDetails;
 @WebServlet("/deletecenter")
 public class DeleteServiceCenter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    @Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		HttpSession session =request.getSession();
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		int centerid = Integer.parseInt(request.getParameter("centerid"));
 		CenterDetails obj1 = new CenterDetails(centerid);
 		CenterDetailsDAOImpl cent = new CenterDetailsDAOImpl();
 		int checkcenterid = cent.checkservicecenterid(obj1);
-		 try {
-				if(checkcenterid!=0)
-				 {
-					PrintWriter writer=response.getWriter();
-					CenterDetails obj2 = new CenterDetails(centerid);
-					CenterDetailsDAOImpl cent2 = new CenterDetailsDAOImpl();
-					  boolean flag = cent2.delete(obj2);
-					  session.setAttribute("deletecenter", true);
-					  if(flag)
-					  {
-						  writer.print("<script type=\"text/javascript\"> alert('Service Center is Deleted'); window.location = 'adminPage.jsp';</script>");
-					  }
-				 }
-				 else
-				 {
-				     throw new CenterNotFoundException(); 		 
-				 }
-			} 
-		 catch (IOException e) 
-		 {
-				e.getCause();
-	     }
-		 catch(CenterNotFoundException e)
-		 {
+		try {
+			if (checkcenterid != 0) {
+				PrintWriter writer = response.getWriter();
+				CenterDetails obj2 = new CenterDetails(centerid);
+				CenterDetailsDAOImpl cent2 = new CenterDetailsDAOImpl();
+				boolean flag = cent2.delete(obj2);
+				session.setAttribute("deletecenter", true);
+				if (flag) {
+					writer.print(
+							"<script type=\"text/javascript\"> alert('Service Center is Deleted'); window.location = 'adminPage.jsp';</script>");
+				}
+			} else {
+				throw new CenterNotFoundException();
+			}
+		} catch (IOException e) {
+			e.getCause();
+		} catch (CenterNotFoundException e) {
 			response.sendRedirect("centerNotFound.jsp");
-		 }	
-		 }
+		}
+	}
 }
